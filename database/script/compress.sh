e24fc327f05ec78158ff3b4d6665ecc8ca2ac492
chunwei <tranway@qq.com>
2020-03-09 00:32:47 -0500

    script for UCI

--- database/script/compress.sh
+++ database/script/compress.sh
@@ -1,9 +1,10 @@
 cd /mnt/hdd-2T-3/chunwei/TimeSeriesDB/database;
+dir=$1
 for comp in fcm dfcm grilla zlib paa fourier snappy deflate gzip bp deltabp;
 do
 	for type in i32 f32 f64;
 		do
-			for file in $(ls $1);
+			for file in $(ls ${dir});
 #			for file in $(ls /mnt/hdd-2T-3/chunwei/TimeSeriesDB/UCRArchive2018/*/*);
 			    do
 if [[ $type == "u32" || $type == "i32" ]]
